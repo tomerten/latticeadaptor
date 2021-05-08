e0b97ede1cec4a923431822ca20c479ccad184c6
Tom Mertens <your.email@whatev.er>
2021-05-08 14:18:18 +0200

    updated conf.pyu

--- docs/conf.py
+++ docs/conf.py
@@ -93,7 +93,7 @@ pygments_style = "sphinx"
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
-plot_include_source
+plot_include_source = True
 # -- Options for HTML output -------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
